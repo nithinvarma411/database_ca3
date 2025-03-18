519fc9314a1d9544c4ac8f6fe5d48f5cf6830d96
nithinvarma <nithinvarma411@gmail.com>
2025-03-18 15:54:25 +0530

    completed CA

--- frontend/src/components/TaskForm.jsx
+++ frontend/src/components/TaskForm.jsx
@@ -1,10 +1,28 @@
 // src/components/TaskForm.jsx
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function TaskForm() {
+    const [task,setTask] = useState('');
+    const [desc, setDesc] = useState('');
+    const addData = async () => {
+        try {
+            const response = await axios.post("http://localhost:5000/tasks", {title: task,description: desc})
+
+            console.log(response);
+            
+        } catch (error) {
+            console.error(error)
+        }
+    }
     return (
         <>
             {/* Task form */}
+            <div>
+                <input type="text" placeholder='add title' value={task} onChange={(e) => setTask(e.target.value)}/>
+                <input type="text" placeholder='add description' value={desc} onChange={(e) => setDesc(e.target.value)}/>
+                <button onClick={addData}>add task</button>
+            </div>
         </>
     );
 }
--- frontend/src/components/TaskList.jsx
+++ frontend/src/components/TaskList.jsx
@@ -1,11 +1,37 @@
 
 // src/components/TaskList.jsx
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function TaskList() {
+    const [task, setTask] = useState([]);
+
+    useEffect(() => {
+        const getList = async (e) =>{
+            try {
+                const response = await axios.get("http://localhost:5000/tasks")
+                setTask(response.data)
+                console.log(task)
+                console.log(response.data)
+            } catch (error) {
+                console.error(error)
+            }
+        }
+        getList()
+    }, [])
     return (
         <>
             {/* Task list */}
+            {
+                task.map((e) => (
+                    <div>
+                        <p>title:--- <b>{e.title}</b></p>
+                        <p>description:--- <b>{e.description}</b></p>
+                        <button>delete</button>
+                        <hr />
+                    </div>
+                ))
+            }
         </>
     );
 }
