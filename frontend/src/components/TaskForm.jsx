82a1c9206bef3a9a9dd92bdf24cbff5ba6cc91e3
nithinvarma <nithinvarma411@gmail.com>
2025-03-18 15:05:05 +0530

    first commit

---
+++ frontend/src/components/TaskForm.jsx
@@ -0,0 +1,12 @@
+// src/components/TaskForm.jsx
+import React from 'react';
+
+function TaskForm() {
+    return (
+        <>
+            {/* Task form */}
+        </>
+    );
+}
+
+export default TaskForm;
---
+++ frontend/src/components/TaskList.jsx
@@ -0,0 +1,13 @@
+
+// src/components/TaskList.jsx
+import React from 'react';
+
+function TaskList() {
+    return (
+        <>
+            {/* Task list */}
+        </>
+    );
+}
+
+export default TaskList;
